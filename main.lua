1b07f0e07a8a5ffa0fc5f4e8e6e5f8115c9f95e3
haohanxinchao-rgb <haohanxinchao@gmail.com>
2026-09-01 18:22:31 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,5 @@
-print("KXH Hub đã chạy thành công!")
-
--- Tạo một thông báo nhỏ trong game để kiểm chứng
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "KXH Hub",
-    Text = "Load thành công từ main.lua!",
+    Text = "Đã kết nối thành công tới main.lua!",
     Duration = 5
 })
